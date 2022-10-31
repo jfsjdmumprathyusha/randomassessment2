9853a0fcbe07730b8f6474aea2d4575f0498862d
prathyusha <prathyushaprathyu.ce@gmail.com>
2022-10-31 11:24:22 +0530

    random number program assessment2 on git

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        {
+            Math.random();
+        }
+                {
+                    Random random = new Random();
+                    int x = random.nextInt();
+                    for (int i = 0; i <= 10; i++) {
+
+                        System.out.println("the number is ");
+                        System.out.println(random.nextInt(11));
+                    }
+                }
+            }
+
+        }
